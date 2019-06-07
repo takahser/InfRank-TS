2878374ae2c2ef43ff21e2a2bfeb7ce2f6e9ef45
Seraya Takahashi <seraya.takahashi@swisscom.com>
2019-06-07 15:14:45 +0200

    Exported types

--- src/domain/types.ts
+++ src/domain/types.ts
@@ -3,30 +3,30 @@
  */
 // source ~ u_i
 // target ~ u_j
-enum AssociationType {
+export enum AssociationType {
   Following, // source follows target
   Retweeting, // source retweets target
   Metioning, // source mentions target
 }
 
-interface Author {
+export interface Author {
   id: string,
   nickname: string,
 }
 
-interface Association {
+export interface Association {
   source: Author,
   target: Author,
   label: AssociationType,
 }
 
-interface Tweet {
+export interface Tweet {
   id: string,
   author: Author,
   retweeter: Author[], // retweeter := all author that retweeted >= 1 tweet of the author
 }
 
-interface AuthorRank {
+export interface AuthorRank {
   author: Author,
   rank: number,
 }
